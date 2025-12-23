e8c94546acfc0213652c6385c854c60dfd4a7055
richiex-jesus <matiasrichard300@gmail.com>
2025-12-23 08:53:14 +0800

    updated js file for RPS basic UI

--- javascript.js
+++ javascript.js
@@ -1,104 +1,132 @@
 function getComputerChoice() {
 
-let numberGenerated = Math.floor(Math.random() * 30) + 1;
+let numberGenerated = Math.floor(Math.random() * 3) + 1;
 
-    const computer = document.getElementById('computer-choice');
-
-    if (numberGenerated >= 1 && numberGenerated <= 10) {
-        const ComputerChoiceRock = document.createElement('div');
-        ComputerChoiceRock.textContent = "the computer has chosen rock!";
-        ComputerChoiceRock.style.border = "1px solid black";
-        computer.appendChild(ComputerChoiceRock);
+    if (numberGenerated == 1) {
+        const computer = document.getElementById('computer-choice');
+        computer.textContent = "the computer has chosen rock!";
         let computerChoice = "rock";
         return computerChoice;
-    } else if (numberGenerated > 10 && numberGenerated <= 20) {
-        const ComputerChoicePaper = document.createElement('div');
-        ComputerChoicePaper.textContent = "the computer has chosen paper!";
-        ComputerChoicePaper.style.border = "1px solid black";
-        computer.appendChild(ComputerChoicePaper);
+    } else if (numberGenerated == 2) {
+        const computer = document.getElementById('computer-choice');
+        computer.textContent = "the computer has chosen paper!";
         let computerChoice = "paper";
         return computerChoice;
-    } else if (numberGenerated > 20 && numberGenerated <= 30) {
-        const ComputerChoiceScissors = document.createElement('div');
-        ComputerChoiceScissors.textContent = "the computer has chosen scissors!";
-        ComputerChoiceScissors.style.border = "1px solid black";
-        computer.appendChild(ComputerChoiceScissors);
+    } else if (numberGenerated == 3) {
+        const computer = document.getElementById('computer-choice');
+        computer.textContent = "the computer has chosen scissors!";
         let computerChoice = "scissors";
         return computerChoice;
     }
 
 }
 
-function getHumanChoice() {
+let humanScore = 0;
+let computerScore = 0;
 
-    const buttonRock = document.getElementById("rock");
+function trackScore(humanChoice, computerChoice) {
+
+    if ((humanChoice == "rock" && computerChoice == "rock") || (humanChoice == "paper" && computerChoice == "paper") || (humanChoice == "scissors" && computerChoice == "scissors")) {
+        endGame(humanScore, computerScore);
+    } else if ((humanChoice == "rock" && computerChoice == "scissors") || (humanChoice == "paper" && computerChoice == "rock") || (humanChoice == "scissors" && computerChoice == "paper")) {
+        ++humanScore;
+        let humanScoreboard = document.getElementById('human-scoreboard');
+        humanScoreboard.textContent = "Human: " + humanScore;
+        endGame(humanScore, computerScore);
+    } else {
+        ++computerScore;
+        let computerScoreboard = document.getElementById('computer-scoreboard');
+        computerScoreboard.textContent = "Computer: " + computerScore;
+        endGame(humanScore, computerScore);
+    }
+
+}
+
+function startGame() {
+
+    const buttonRock = document.getElementById('rock');
     const buttonPaper = document.getElementById('paper');
     const buttonScissors = document.getElementById('scissors');
+    
     buttonRock.addEventListener("click", (e) => {
-        console.log("rock");
         e.target.style.backgroundColor = "green";
         buttonPaper.style.backgroundColor = "transparent";
         buttonScissors.style.backgroundColor = "transparent";
         let humanChoice = "rock";
-        return humanChoice;
-
+        let computerChoice = getComputerChoice();
+        trackScore(humanChoice, computerChoice);
+        endGame(humanScore, computerScore);
     });
+
     buttonPaper.addEventListener("click", (e) => {
-        console.log("paper");
+        console.log("paper button clicked!");
         e.target.style.backgroundColor = "green";
         buttonRock.style.backgroundColor = "transparent";
         buttonScissors.style.backgroundColor = "transparent";
         let humanChoice = "paper";
-        return humanChoice;
+        let computerChoice = getComputerChoice();
+        trackScore(humanChoice, computerChoice);
+        endGame(humanScore, computerScore);
     });
+    
     buttonScissors.addEventListener("click", (e) => {
-        console.log("scissors");
+        console.log("scissors button clicked!");
         buttonRock.style.backgroundColor = "transparent";
         buttonPaper.style.backgroundColor = "transparent";
         e.target.style.backgroundColor = "green";
         let humanChoice = "scissors";
-        return humanChoice;
+        let computerChoice = getComputerChoice();
+        trackScore(humanChoice, computerChoice);
+        endGame(humanScore, computerScore);
     });
+
+    const scoreboard = document.getElementById('scores');
+    
+    const humanScoreboard = document.createElement('div');
+    humanScoreboard.id = 'human-scoreboard';
+    humanScoreboard.textContent = "Humans: " + humanScore;
+    scoreboard.appendChild(humanScoreboard);
+    
+    const computerScoreboard = document.createElement('div'); // add class later
+    computerScoreboard.id = 'computer-scoreboard';
+    computerScoreboard.textContent = "Computer: " + computerScore;
+    scoreboard.appendChild(computerScoreboard);
+
 
 }
 
-let humanScore = 0;
-let computerScore = 0;
+startGame();
 
-
-function playRound() { 
-    
-    const scoreboard = document.getElementById('scores');
-    const humanScoreboard = document.createElement('div');
-    const computerScoreboard = document.createElement('div');
-    humanScoreboard.textContent = humanScore;
-    computerScoreboard.textContent = computerScore;
-
-    humanChoice = getHumanChoice();
-
-    if (humanChoice == getComputerChoice()) {
-        scoreboard.appendChild(humanScoreboard);
-        scoreboard.appendChild(computerScoreboard);
-    } else if ((humanChoice == "rock" && getComputerChoice() == "scissors") || (humanChoice == "paper" && getComputerChoice() == "rock") || (humanChoice == "scissors" && getComputerChoice() == "paper")) {
-        let newHumanScore = ++humanScore;
-        humanScoreboard.textContent = newHumanScore;
-        scoreboard.appendChild(humanScoreboard);
-        scoreboard.appendChild(computerScoreboard);
-    } else {
-        let newComputerScore = ++computerScore;
-        computerScoreboard.textContent = newComputerScore;
-        scoreboard.appendChild(humanScoreboard);
-        scoreboard.appendChild(computerScoreboard);
+function endGame(humanScore, computerScore) {
+    let gameWinner = document.getElementById('winner');
+    if ((humanScore == 5) && (computerScore < 5)) {
+        let computer = document.getElementById('computer-choice');
+        computer.remove();
+        gameWinner.textContent = "The human has beaten the computer!";
+        let container = document.getElementById('container');
+        container.replaceChildren();
+        restartGame();
+    } else if ((computerScore == 5) && (humanScore < 5)) {
+        let computer = document.getElementById('computer-choice');
+        computer.remove();
+        gameWinner.textContent = "The computer has beaten the human!"
+        let container = document.getElementById('container');
+        container.replaceChildren();
+        restartGame();
+    } else if (humanScore < 5 && computerScore < 5) {
+        gameWinner.textContent = "";
     }
 }
 
-playRound();
+function restartGame() {
+    let container = document.getElementById('container');
+    let restartButton = document.createElement('button');
+    restartButton.id = "restart-button";
+    restartButton.textContent = "Restart";
+    container.appendChild(restartButton);
+    restartButton.addEventListener('click', (e) => {
+       location.reload();
+    })
+}
 
-// option 1
-// document.addEventListener("DOMContentLoaded", playRound)
 
-// option 2: add 'defer' attribute
-
-// option 3: move script tag inside body and put at bottom
-
-// playRound();
